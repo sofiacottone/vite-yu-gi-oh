f912d07ac5988954663f71acfcf8aa07a36ed931
sofiacottone <cottonesofia@outlook.it>
2024-04-15 16:01:45 +0200

    feat: add header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+
+</script>
+
+<template>
+    <header>
+        <div class="container-fluid">
+            <div class="d-flex align-items-center">
+                <div class="ms-logo-wrapper p-3">
+                    <img class="img-fluid"
+                        src="https://vignette1.wikia.nocookie.net/yugiohenespanol/images/a/aa/Yugioh_anime_logo.jpg/revision/latest?cb=20130310042737&path-prefix=es"
+                        alt="yu-gi-og logo">
+                </div>
+                <div class="fs-3">Yu-Gi-Oh Api</div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style scoped lang="scss">
+.ms-logo-wrapper {
+    width: 100px;
+}
+</style>
