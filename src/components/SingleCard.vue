d8c5f5cbe5ba35684d310b37b50f8b432d073a0d
sofiacottone <cottonesofia@outlook.it>
2024-04-15 16:57:20 +0200

    feat: add single card component

---
+++ src/components/SingleCard.vue
@@ -0,0 +1,35 @@
+<script>
+
+export default {
+    name: 'SingleCard',
+}
+
+</script>
+
+<template>
+
+    <div class="ms-single-card text-center">
+        <img src="https://images.ygoprodeck.com/images/cards_small/34541863.jpg" alt="">
+        <div class="text-white text-uppercase h5 py-3">
+            name
+        </div>
+        <div class="pb-4">
+            Species
+        </div>
+
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables' as *;
+
+.ms-single-card {
+    width: calc(100% / 5 - 20px);
+    background-color: $main-orange;
+
+    img {
+        width: 100%;
+    }
+}
+</style>
