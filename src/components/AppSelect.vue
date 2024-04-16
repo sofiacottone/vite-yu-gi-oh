c05ffe3a13e7b8eca7ceaade54e147fbe340efec
sofiacottone <cottonesofia@outlook.it>
2024-04-16 17:12:48 +0200

    feat: add select component

---
+++ src/components/AppSelect.vue
@@ -0,0 +1,44 @@
+<script>
+import { store } from '../store.js'
+
+export default {
+    name: 'AppSelect',
+    data() {
+        return {
+            store
+        }
+    }
+
+}
+
+</script>
+
+<template>
+    <div class="pt-4 ps-5">
+        <select v-model="store.selectedItem" class="border-1 rounded p-1">
+            <option value="">Select Archetype</option>
+
+            <!-- options -->
+            <option v-for="archetype in store.archetypes" :value="archetype.archetype_name">{{ archetype.archetype_name
+                }}
+            </option>
+
+        </select>
+
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables' as *;
+
+div {
+    background-color: $main-orange;
+
+    select {
+        outline-color: $main-orange;
+        border: 2px solid $main-orange;
+    }
+
+}
+</style>
